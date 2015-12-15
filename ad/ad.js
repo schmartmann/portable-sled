83deb1aa509ecd9295a70f75affc020dfa3b2dec
perrypapadopoulos <perry.papadopoulos@gmail.com>
2015-12-14 20:41:04 -0500

    cleanup of creative template files

--- ad/ad.js
+++ ad/ad.js
@@ -1,18 +1,68 @@
 Sled.plugins = {
-	onLoad: function(args) {
-		var adWidth = window.screen.availWidth;
-		args.ad.style.height = adWidth + 'px';
-	},
-	onImpression: function(args) {
-	},
-	onScroll: function(args) {
-	},
-	onFullView: function(args) {
-	},
-	onClick: function(args) {
-	},
-	onSwipe: function(args) {
-	},
-	onExit: function(args) {
-	}
+	// onLoad: function(args) {
+	// 	console.log("Ad-specific onLoad");
+	// },
+	// onBeforeImpression: function() {
+	// 	console.log("Ad-specific onBeforeImpression");
+	// },
+	// onBeforeImpressionGesture: function() {
+	// 	console.log("Ad-specific onBeforeImpressionGesture");
+	// },
+	// onImpression: function(args) {
+	// 	console.log("Ad-specific onImpression");
+	// },
+	// scrollPoints: [
+	// 	{
+	// 		'entry': 25, // % in view
+	// 		'function': function() {
+	// 			console.log("Ad-specific scroll point entry at 25%");
+	// 		}
+	// 	}, {
+	// 		'center': true, // if passes the centerline, so >= in that direction
+	// 		'function': function() {
+	// 			console.log("Ad-specific scroll point at center");
+	// 		}
+	// 	},{
+	// 		'exit': 25, // % in view
+	// 		'function': function() {
+	// 			console.log("Ad-specific scroll point exit at 25%");
+	// 		}
+	// 	}
+	// ],
+	// onFullView: function(args) {
+	// 	console.log("Ad-specific onFullView");
+	// },
+	// onClick: function(args) {
+	// 	console.log("Ad-specific onClick");
+	// },
+	// onSwipe: function(args) {
+	// 	console.log("Ad-specific onSwipe");
+	// },
+	// onExit: function(args) {
+	// 	console.log("Ad-specific onExit");
+	// },
+	// onLock: function() {
+	// 	console.log("Potentially used on video sticker locking functionality");
+	// },
+	// onVideoQuartile: function(args) {
+	// 	switch(args.quartile) {
+	// 		case 0:
+	// 			console.log('Ad-specific onVideoQuartile-0');
+	// 			break;
+	// 		case 1:
+	// 			console.log('Ad-specific onVideoQuartile-1');
+	// 			break;
+	// 		case 2:
+	// 			console.log('Ad-specific onVideoQuartile-2');
+	// 			break;
+	// 		case 3:
+	// 			console.log('Ad-specific onVideoQuartile-3');
+	// 			break;
+	// 		case 4:
+	// 			console.log('Ad-specific onVideoQuartile-4');
+	// 			break;
+	// 		default:
+	// 			console.log(args.quartile);
+	// 	}
+	// }
 };
